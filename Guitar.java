490de74537e805ab01b140a0b1b22b315a7dc827
zahmis <meda829ratteru86@gmail.com>
2021-06-18 19:22:44 +0900

    fix

--- Guitar.java
+++ Guitar.java
@@ -1,18 +1,13 @@
 public class Guitar {
 
-    private String serialNumber, builder, model, type, backWood, topWood;
+    private String serialNumber;
     private double price;
+    GuitarSpec spec;
   
-    public Guitar(String serialNumber, double price,
-                  String builder, String model, String type,
-                  String backWood, String topWood) {
+    public Guitar(String serialNumber, double price, GuitarSpec spec) {
       this.serialNumber = serialNumber;
       this.price = price;
-      this.builder = builder;
-      this.model = model;
-      this.type = type;
-      this.backWood = backWood;
-      this.topWood = topWood;
+      this.spec = spec;
     }
   
     public String getSerialNumber() {
@@ -22,23 +17,13 @@ public class Guitar {
     public double getPrice() {
       return price;
     }
+  
     public void setPrice(float newPrice) {
       this.price = newPrice;
     }
-    public String getBuilder() {
-      return builder;
-    }
-    public String getModel() {
-      return model;
-    }
-    public String getType() {
-      return type;
-    }
-    public String getBackWood() {
-      return backWood;
-    }
-    public String getTopWood() {
-      return topWood;
+  
+    public GuitarSpec getSpec() {
+      return spec;
     }
   }
   
