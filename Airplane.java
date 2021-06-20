1e09b674fa4b947acf1569c3f27c042ba02b279b
zahmis <meda829ratteru86@gmail.com>
2021-06-21 05:29:10 +0900

    create Airplane class

---
+++ Airplane.java
@@ -0,0 +1,14 @@
+public class Airplane {
+    private int speed;
+
+    public Airplane(){
+    }
+
+    public void setSpeed(int speed){
+      this.speed = speed;
+    }
+
+    public int getSpeed(){
+        return speed;
+    }
+}
