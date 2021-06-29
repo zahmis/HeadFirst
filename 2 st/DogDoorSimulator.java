561da519b2d66866267da7f6679136d9e68fa4e2
zahmis <meda829ratteru86@gmail.com>
2021-06-29 20:12:07 +0900

    fix simulator

--- 2 st/DogDoorSimulator.java
+++ 2 st/DogDoorSimulator.java
@@ -8,13 +8,19 @@ public class DogDoorSimulator {
       remote.pressButton();
   
       System.out.println("\nFido has gone outside...");
-      remote.pressButton();
-  
       System.out.println("\nFido's all done...");
+  
+      try {
+        Thread.currentThread().sleep(10000);
+      } catch (InterruptedException e) { }
+  
+      System.out.println("...but he's stuck outside!");
+  
+      System.out.println("\nFido starts barking...");
+      System.out.println("...so Gina grabs the remote control.");
       remote.pressButton();
   
       System.out.println("\nFido's back inside...");
-      remote.pressButton();
     }
   }
   
