0889d17f9f82d5c3dc4dc917e6249f21068f923e
zahmis <meda829ratteru86@gmail.com>
2021-07-13 17:41:46 +0900

    add BarkRecognizer.java

---
+++ 2 st/BarkRecognizer.java
@@ -0,0 +1,15 @@
+public class BarkRecognizer {
+
+    private DogDoor door;
+  
+    public BarkRecognizer(DogDoor door) {
+      this.door = door;
+    }
+  
+    public void recognize(String bark) {
+      System.out.println("   BarkRecognizer: Heard a '" +
+          bark + "'");
+      door.open();
+    }
+  }
+  
