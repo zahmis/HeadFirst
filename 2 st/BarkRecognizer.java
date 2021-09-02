f8e1b3c79441ee165aa3afdd1ff632c8e057f990
zahmis <meda829ratteru86@gmail.com>
2021-09-02 20:15:11 +0900

    fix barkrecognizer

--- 2 st/BarkRecognizer.java
+++ 2 st/BarkRecognizer.java
@@ -1,3 +1,6 @@
+import java.util.Iterator;
+import java.util.List;
+
 public class BarkRecognizer {
 
     private DogDoor door;
@@ -6,9 +9,25 @@ public class BarkRecognizer {
       this.door = door;
     }
   
-    public void recognize(String bark) {
+    public void recognize(Bark bark) {
       System.out.println("   BarkRecognizer: Heard a '" +
           bark + "'");
+      
+      List<Bark> allowedBarks = door.getAllowedBarks();
+
+      for (Iterator<Bark> i = allowedBarks.iterator(); i.hasNext(); ) {
+        Bark allowedBark = (Bark)i.next();
+
+        if (allowedBark.equals(bark)) {
+          door.open();
+          return;
+        }
+      } 
+
+      System.out.println("This dog is not allowed.");
+
+
+
       door.open();
     }
   }
